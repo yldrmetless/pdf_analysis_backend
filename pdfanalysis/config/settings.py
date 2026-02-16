f4e39d3872d9dd076fe85d81cf4794b82015967d
Metehan Yıldırım <81328549+yldrmetless@users.noreply.github.com>
2026-02-16 15:43:29 +0300

    update

--- pdfanalysis/config/settings.py
+++ pdfanalysis/config/settings.py
@@ -116,6 +116,9 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
